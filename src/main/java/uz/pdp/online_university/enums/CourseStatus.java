d718791664613b6399eacac014137a0aff01dab5
xusniya <118680313+xusniya@users.noreply.github.com>
2026-03-12 07:22:56 +0500

    Create CourseStatus.java
    - Added CourseStatus enum to support lifecycle states:
      - DRAFT
      - IN_REVIEW
      - APPROVED
      - PUBLISHED
      - REJECTED
      - ARCHIVED
      - REAPPROVAL_REQUIRED

---
+++ src/main/java/uz/pdp/online_university/enums/CourseStatus.java
@@ -0,0 +1,13 @@
+package uz.pdp.online_university.enums;
+
+public enum CourseStatus {
+
+    DRAFT,
+    IN_REVIEW,
+    APPROVED,
+    PUBLISHED,
+    REJECTED,
+    ARCHIVED,
+    REAPPROVAL_REQUIRED
+
+}
