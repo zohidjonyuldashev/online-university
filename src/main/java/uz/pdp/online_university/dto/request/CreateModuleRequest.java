2b60f5c1f707b69c5e7c257cd5f78be3135ea4f6
xusniya <118680313+xusniya@users.noreply.github.com>
2026-03-12 23:40:27 +0500

    Add CreateModuleRequest DTO with validation constraints

---
+++ src/main/java/uz/pdp/online_university/dto/request/CreateModuleRequest.java
@@ -0,0 +1,22 @@
+package uz.pdp.online_university.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateModuleRequest {
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 2, max = 255, message = "Title must be between 2 and 255 characters")
+    private String title;
+
+    @Size(max = 1000, message = "Description must not exceed 1000 characters")
+    private String description;
+}
